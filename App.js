4502ce18aa812e52a7ff85c1f2d90ee0ec9a15f5
YU WANG <yuwang10@upenn.edu>
2024-08-08 11:32:04 +0800

    a6 commit

--- App.js
+++ App.js
@@ -5,15 +5,25 @@ import cors from "cors";
 import CourseRoutes from "./Lab5/Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Lab5/Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Lab5/Kanbas/Assignments/routes.js';
+import mongoose from "mongoose";
+// import UserRoutes from "./Users/routes.js";
+import UserRoutes from "./User/routes.js";
+import "dotenv/config";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+
 const app = express()         
 app.use(express.json());
 app.use(cors());  
 app.use(express.json()); 
+
 ModuleRoutes(app);
 
 
 CourseRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Hello(app)
 Lab5(app);
 
